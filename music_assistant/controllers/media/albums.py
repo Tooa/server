ff905d03674c1b76106b4ad1bc2b6b8208650c8e
Jozef Kruszynski <jozef+github@krush.at>
2025-12-03 07:56:03 +0100

    style(genre): mypy and  ruff fixes

--- music_assistant/controllers/media/albums.py
+++ music_assistant/controllers/media/albums.py
@@ -111,6 +111,7 @@ class AlbumsController(MediaControllerBase[Album]):
         provider: str | list[str] | None = None,
         extra_query: str | None = None,
         extra_query_params: dict[str, Any] | None = None,
+        genre_filter: list[int] | None = None,
         album_types: list[AlbumType] | None = None,
     ) -> list[Album]:
         """Get in-database albums.
@@ -168,6 +169,7 @@ class AlbumsController(MediaControllerBase[Album]):
             extra_query_parts=extra_query_parts,
             extra_query_params=extra_query_params,
             extra_join_parts=extra_join_parts,
+            genre_filter=genre_filter,
         )
 
         # Calculate how many more items we need to reach the original limit
@@ -195,6 +197,7 @@ class AlbumsController(MediaControllerBase[Album]):
                 extra_query_parts=extra_query_parts,
                 extra_query_params=extra_query_params,
                 extra_join_parts=extra_join_parts,
+                genre_filter=genre_filter,
             ):
                 # prevent duplicates (when artist is also in the title)
                 if album.uri not in existing_uris:
